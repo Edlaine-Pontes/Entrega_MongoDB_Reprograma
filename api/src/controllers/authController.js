f98c3714abf4205d10bee14da6a6014a9be373e1
Edlaine Pontes <edlaine.psico@gmail.com>
2020-11-06 21:00:13 -0300

    inicio api usuarios

---
+++ api/src/controllers/authController.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const User = require('../models/User');
+
+const router = express.Router();
+
+router.post('/register', async (req, res) => {
+
+    const { email } = req.body;
+
+    try{
+        if (await User.findOne({ email }))
+            return res.status(400).send({ error: 'Usuario ja existe'});
+
+        const user = await User.create(req.body);
+        user.password = undefined;
+        return res.send({ user });
+
+    } catch (err) {
+        return res.status(400).send({error:'Falha no Registro'});
+    }
+});
+
+module.exports = app => app.use('/auth', router);
